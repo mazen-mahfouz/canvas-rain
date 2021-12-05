1cb227b8a723a4ef6cd3f6281812d65d8a46f761
mazen-mahfouz <75842519+mazen-mahfouz@users.noreply.github.com>
2021-12-06 00:10:10 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -7,16 +7,6 @@ let pI = Math.PI / 180;
 
 var ctx = canvas.getContext('2d');
 
-///////////////////////////////////////////////////////////////////////////////
-let true_flase = false;
-let array = ["#34495e", "#e74c3c", '#2ecc71', '#9b59b6', '#f1c40f', '#1abc9c'];
-
-canvas.onmousedown = function (e) {
-    true_flase = true;
-}
-canvas.onmouseup = function (e) {
-    true_flase = false;
-}
 
 let x = [];
 let y = [];
@@ -24,98 +14,92 @@ let dx = [];
 let dy = [];
 let color = [];
 let size = [];
-let count;
+let count = 50;
 let opacity = [];
 let color_pattern = ["#34495e", "#e74c3c", '#2ecc71', '#9b59b6', '#f1c40f', '#1abc9c'];
 let r = [];
 let size_event;
 let size_min = [];
 let pos;
-resize()
-function resize() {
-    if (window.innerWidth < 900) {
-        count = 150;
-        size_event = 30;
-        pos = 80;
-    } else {
-        count = 500;
-        size_event = 50;
-        pos = 120;
-    }
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-}
-window.addEventListener('resize', resize, false);
+var grd = [];
+let effect = [];
+let effect_r = [];
+let t_f = true;
+let rain = document.getElementById('rain');
+let br2 = document.getElementById('br2');
 
 for (let i = 0; i < count; i++) {
-    size_min[i] = (Math.random() * (5 - 1)) + 1;
-    size[i] = size_min[i];
-    x[i] = Math.random() * (innerWidth - size[i] * 2) + size[i];
-    y[i] = Math.random() * (innerHeight - size[i] * 2) + size[i];
-    dx[i] = (Math.random() - 0.5) * 2;
-    dy[i] = (Math.random() - 0.5) * 2;
-    color[i] = color_pattern[Math.floor(Math.random() * 5)]
-    r[i] = 10.1;
-    opacity[i] = .3;
+    x[i] = Math.random() * (window.innerWidth - 0) + 0;
+    y[i] = -Math.random() * ((window.innerHeight * 1) - 0) + 0;
+    dx[i] = (Math.random() - 0) * 2;
+    dy[i] = (Math.random() + 1) * 3;
+    opacity[i] = (Math.random() + .2) * .9;
+    effect_r[i] = .5;
+
+    grd[i] = ctx.createLinearGradient(0, y[i], 0, y[i] + 20);
+    grd[i].addColorStop(0, "#ffffff38");
+    grd[i].addColorStop(1, "white");
+    color[i] = grd[i];
+
 }
 
-let mouseX;
-let mouseY;
-canvas.onmousemove = function (event) {
-    mouseX = event.pageX;
-    mouseY = event.pageY;
-}
-
-canvas.ontouchend = function () {
-    if (window.innerWidth < 900) {
-        document.body.requestFullscreen();
-    }
-}
-
-canvas.ontouchmove = function (event) {
-    mouseX = event.touches[0].pageX;
-    mouseY = event.touches[0].pageY;
-}
-
-function new_circle() {
-
+// rain.play()
+let h = window.innerHeight;
+function new_rain() {
     for (let i = 0; i < count; i++) {
-        if (x[i] + size[i] > innerWidth || x[i] - size[i] < 0) {
-            dx[i] = -dx[i];
-        }
 
-        if (y[i] + size[i] > innerHeight || y[i] - size[i] < 0) {
-            dy[i] = -dy[i];
-        }
-
-
-        ctx.beginPath();
-        ctx.arc(x[i], y[i], size[i], 0, Math.PI * 2, false);
-        ctx.fillStyle = color[i];
         ctx.globalAlpha = opacity[i];
+        grd[i] = ctx.createLinearGradient(0, y[i], 0, y[i] + 20);
+        grd[i].addColorStop(0, "#ffffff38");
+        grd[i].addColorStop(1, "white");
+        ctx.fillStyle = color[i];
+        ctx.beginPath();
+        ctx.rect(x[i], y[i], 2, 20)
         ctx.fill()
-        x[i] += dx[i];
-        y[i] += dy[i];
+        y[i] += dy[i]
 
-        for (let i = 0; i < count; i++) {
-            if (mouseX > x[i] - pos && mouseX < x[i] + pos && mouseY > y[i] - pos && mouseY < y[i] + pos) {
-                if (size[i] < size_event) {
-                    r[i] = r[i] + .001 + .001;
-                    size[i] = r[i];
-                    opacity[i] = .8;
-                }
+        if (y[i] > window.innerHeight - 100) {
+            if (effect_r[i] < 20) {
+                ctx.beginPath()
+                ctx.strokeStyle = 'white';
+                ctx.lineWidth = .7;
+                effect_r[i] = effect_r[i] + .4;
+                ctx.ellipse(x[i], window.innerHeight - 100 + 20, effect_r[i] / 3.5, effect_r[i] + effect_r[i] + effect_r[i], -90 * pI, 0, 360 * pI, false)
+                ctx.stroke();
+                color[i] = '#20202000';
             } else {
-                if (size[i] > size_min[i]) {
-                    r[i] = r[i] - .001 - .001;
-                    size[i] = r[i];
-                    opacity[i] = .3;
-                }
+                color[i] = grd[i];
+                effect_r[i] = .1;
+                dy[i] = (Math.random() + 1) * 3;
+                y[i] = -Math.random() * ((window.innerHeight * 1) - 0) + 0;
             }
         }
     }
 }
 
-setInterval(() => {
-    ctx.clearRect(0, 0, innerWidth, innerHeight);
-    new_circle()
-}, 0);
+function start() {
+    document.getElementById('start').style.display = 'none';
+    canvas.style.background = grd;
+    setInterval(() => {
+        ctx.clearRect(0, 0, innerWidth, innerHeight);
+        new_rain()
+    }, 0);
+
+    let array_br2 = ['linear-gradient(179deg, #91b6e5, rgb(17, 17, 25))', 'linear-gradient(179deg, rgb(179 179 179), rgb(17, 17, 25))', 'linear-gradient(179deg, #c17b7b, rgb(17, 17, 25))']
+    setInterval(() => {
+        let random = Math.floor(Math.random() * 3);
+
+        canvas.style.backgroundImage = array_br2[random];
+        setTimeout(() => {
+            canvas.style.backgroundImage = 'linear-gradient(182deg, #4c545e, #111119)';
+        }, 100);
+
+    }, 3000);
+    rain.play()
+    setInterval(fun_br2, 3000)
+}
+
+function fun_br2() {
+    br2.currentTime = 0;
+    br2.play()
+}
